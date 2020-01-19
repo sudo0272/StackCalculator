ece98baafe6c3b3ae02336410fa58945cae9861c
홍세정 <sudo0272@gmail.com>
2020-01-19 14:35:00 +0900

    added convertInfixExpressionToPostfixExpression

---
+++ lib/convertInfixExpressionToPostfixExpression.cpp
@@ -0,0 +1,61 @@
+#include <vector>
+#include <string>
+#include "convert_expression.h"
+
+/**
+ * convert infix-expression to postfix-expression
+*/
+void convertInfixExpressionToPostfixExpression(std::string *result, std::string expression, unsigned int *i) {
+    std::vector<char> signs;
+
+    while (expression[*i] != ')') {
+        switch (expression[*i]) {
+            case 'A' ... 'Z':
+                (*result) += expression[*i];
+
+                break;
+            
+            case '*':
+            case '/':
+                while (!signs.empty() && (signs.back() == '*' || signs.back() == '/')) {
+                    (*result) += signs.back();
+
+                    signs.pop_back();
+                }
+
+                signs.push_back(expression[*i]);
+
+                break;
+            
+            case '+':
+            case '-':
+                while (!signs.empty() && (signs.back() == '+' || signs.back() == '-' || signs.back() == '*' || signs.back() == '/')) {
+                    (*result) += signs.back();
+
+                    signs.pop_back();
+                }
+
+                signs.push_back(expression[*i]);
+
+                break;
+            
+            case '(':
+                (*i)++;
+
+                convertInfixExpressionToPostfixExpression(result, expression, i);
+                
+                break;
+
+            default:
+                break;
+        }
+
+        (*i)++;
+    }
+
+    while (!signs.empty()) {
+        (*result) += signs.back();
+        
+        signs.pop_back();
+    }
+}
---
+++ lib/convert_expression.h
@@ -0,0 +1,6 @@
+#pragma once
+#define _CONVERT_EXPRESSION_H_
+
+#include <string>
+
+void convertInfixExpressionToPostfixExpression(std::string *result, std::string expression, unsigned int *i)
